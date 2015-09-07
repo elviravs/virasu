327bdefa277d8249327abada9b8b598ab83b0c1e
elviravs <cynthiaevs@gmail.com>
2015-09-07 18:40:58 +0700

    vira

---
+++ stardust.hs
@@ -0,0 +1,15 @@
+module Two where
+
+import Data.List
+
+super a b = a*b
+
+fst' (a, b) = a
+
+-- fst, snd, head, tail, max, min, and, or, length, con
+
+snd' (a, b) = b
+
+null x == [] = true
+otherwise = false
+
